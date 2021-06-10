d06341ba46097ea943c1e4b74a0f242414d61e33
chan-1807 <81011305+chan-1807@users.noreply.github.com>
2021-06-10 22:07:45 +0530

    Update c1.java

--- Git/src/Package1/c1.java
+++ Git/src/Package1/c1.java
@@ -2,11 +2,6 @@ package Package1;
 
 public class c1 {
 
-
-	public static void main(String[] args) {
-		System.out.println("UPDATED C! class in eclipse");
-	}
-
-syso(updated c1 class in git )
+chandru form GIT
 
 }
